92bba8ffb53eee6b15314d6bd1178482844f328f
Mahmouud74 <mahmouudmoohamed@gmail.com>
2023-05-07 22:29:58 +0200

    add appfile

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+</script>
+
+<template class="">
+  <Header v-if="!$route.meta.hideHeader"></Header>
+  <router-view ></router-view>
+</template>
+
+<style scoped>
+
+</style>
